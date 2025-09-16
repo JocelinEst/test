6632868d401c8f8fce4b55eef8b79812ded5c4f0
Jocelin <estellle.cuteweb@gmail.com>
2025-09-16 19:14:57 +0300

    sdf
    sdf

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 3, b = 5;
+            int c = a + b;
+        }
+    }
+}
