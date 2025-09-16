f62036ba4458adf5a8e28219769acb865b2c59e0
Jocelin <estellle.cuteweb@gmail.com>
2025-09-16 19:20:12 +0300

    sdfds

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             int a = 3, b = 5;
-            int c = a + b;
+            int c = a + b+2;
         }
     }
 }
